2877d7966891801645d8ae06334978e4df1c0fa2
František Sabovčik <sitnarf@gmail.com>
2019-11-13 10:40:51 +0100

    fs api

--- src/index.js
+++ src/index.js
@@ -1,19 +1,20 @@
 function rejectWithMessage(error) {
     return Promise.reject(error.message);
 }
+const fs = require("fs");
 
 export default (key, replacer, reviver) => ({
     load() {
-        return new Promise((resolve) => {
-            const jsonState = localStorage.getItem(key);
+        return new Promise(resolve => {
+            var jsonState = fs.readFileSync("storage");
             resolve(JSON.parse(jsonState, reviver) || {});
         }).catch(rejectWithMessage);
     },
 
     save(state) {
-        return new Promise((resolve) => {
+        return new Promise(resolve => {
             const jsonState = JSON.stringify(state, replacer);
-            localStorage.setItem(key, jsonState);
+            fs.writeFileSync("storage", jsonState);
             resolve();
         }).catch(rejectWithMessage);
     }
